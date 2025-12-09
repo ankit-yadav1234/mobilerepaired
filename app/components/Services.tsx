3daa82d452dc205b7be983ca31468eab6df2ac78
Ankit Yadav <ankitybarahi@gmail.com>
2025-12-09 18:21:49 +0530

    chore: update next.js to version 16.0.8

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -1,11 +1,27 @@
-import ServiceCard from './ServiceCard'
+import ServiceCard from "./ServiceCard";
 
 const DATA = [
-  { title: 'iPhone', desc: 'Cracked screens, water damage, battery problems, and more.', img: 'iphone.svg' },
-  { title: 'iPad', desc: 'Cracked screens, battery problems, and software issues.', img: 'ipad.svg' },
-  { title: 'Android', desc: 'We can do complex repairs like motherboard replacements.', img: 'android.svg' },
-  { title: 'Tablet', desc: 'Fast and cost-effective solutions for all kinds of tablets.', img: 'tablet.svg' },
-]
+  {
+    title: "iPhone",
+    desc: "Cracked screens, water damage, battery problems, and more.",
+    img: "iphone.svg",
+  },
+  {
+    title: "iPad",
+    desc: "Cracked screens, battery problems, and software issues.",
+    img: "ipad.svg",
+  },
+  {
+    title: "Android",
+    desc: "We can do complex repairs like motherboard replacements.",
+    img: "android.svg",
+  },
+  {
+    title: "Tablet",
+    desc: "Fast and cost-effective solutions for all kinds of tablets.",
+    img: "tablet.svg",
+  },
+];
 
 export default function Services() {
   return (
@@ -14,5 +30,5 @@ export default function Services() {
         <ServiceCard key={item.title} {...item} />
       ))}
     </>
-  )
+  );
 }
